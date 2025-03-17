b5c303b4bb426e8c42a5840822dc9339f1baa15b
aanshiksinghtomar <aanshiksinghtomar@gmail.com>
2025-03-18 04:19:28 +0530

    Data Added by Ashish

--- assets/js/script.js
+++ assets/js/script.js
@@ -156,6 +156,9 @@ document.addEventListener("DOMContentLoaded", () => {
    const dotsContainer = document.querySelector(".dots");
    const buttonsContainer = document.querySelector(".buttons");
    const eventText = document.getElementById("eventText");
+   const eventNum = document.getElementById("eventNum");
+   const eventName = document.getElementById("eventName");
+
    const totalSlides = 3;
    let currentIndex = 1;
    let autoSlideInterval;
@@ -163,7 +166,9 @@ document.addEventListener("DOMContentLoaded", () => {
    // Event Names & Descriptions
    const slideData = [
       { name: "Opening Ceremony", text: "Gear up for an electrifying event at IIIT's fest! Teams of 4 will battle in two rounds: a quiz to secure a franchise spot, followed by a high-stakes player auction. With 100 crores, build your dream team of 12 players while balancing Indian and overseas stars. Master the bidding war, strategize your picks, and aim for the highest team rating to win. Will you outsmart the competition? Join the thrill and showcase your cricketing genius!" },
+
       { name: "Tech Conference", text: "Unleash your creativity in this week-long Instagram marketing showdown! Teams of 4-5 will craft and run a dynamic campaign for a given product, posting daily content, reels, and stories to maximize engagement. After 6 days of strategizing and engaging, teams will pitch their campaign results to a panel of judges. Showcase your skills in analytics, content creation, and storytelling to win exciting rewards! Join the challenge and prove your marketing prowess!" },
+
       { name: "Closing Gala", text: "Gear up for an electrifying event at IIIT's fest! Teams of 4 will battle in two rounds: a quiz to secure a franchise spot, followed by a high-stakes player auction. With 100 crores, build your dream team of 12 players while balancing Indian and overseas stars. Master the bidding war, strategize your picks, and aim for the highest team rating to win. Will you outsmart the competition? Join the thrill and showcase your cricketing genius!" }
    ];
 
@@ -189,6 +194,8 @@ document.addEventListener("DOMContentLoaded", () => {
       // Update Event Description
       const dataIndex = (currentIndex === 0) ? totalSlides - 1 : (currentIndex - 1) % totalSlides;
       eventText.textContent = slideData[dataIndex].text;
+      eventName.textContent = slideData[dataIndex].name;
+      eventNum.textContent = `# ${currentIndex} / ${totalSlides}`;
    }
 
    function createControls() {
