0274ec8d3ac6a9051207a22adbc3e1f4276fa7e7
aanshiksinghtomar <aanshiksinghtomar@gmail.com>
2025-03-18 18:02:27 +0530

    Event Page design update

--- assets/js/script.js
+++ assets/js/script.js
@@ -159,7 +159,7 @@ document.addEventListener("DOMContentLoaded", () => {
    const eventNum = document.getElementById("eventNum");
    const eventName = document.getElementById("eventName");
 
-   const totalSlides = 3;
+   const totalSlides = 5;
    let currentIndex = 1;
    let autoSlideInterval;
 
@@ -169,7 +169,11 @@ document.addEventListener("DOMContentLoaded", () => {
 
       { name: "Event 2", text: "Unleash your creativity in this week-long Instagram marketing showdown! Teams of 4-5 will craft and run a dynamic campaign for a given product, posting daily content, reels, and stories to maximize engagement. After 6 days of strategizing and engaging, teams will pitch their campaign results to a panel of judges. Showcase your skills in analytics, content creation, and storytelling to win exciting rewards! Join the challenge and prove your marketing prowess!" },
 
-      { name: "Event 3", text: "Gear up for an electrifying event at IIIT's fest! Teams of 4 will battle in two rounds: a quiz to secure a franchise spot, followed by a high-stakes player auction. With 100 crores, build your dream team of 12 players while balancing Indian and overseas stars. Master the bidding war, strategize your picks, and aim for the highest team rating to win. Will you outsmart the competition? Join the thrill and showcase your cricketing genius!" }
+      { name: "Event 3", text: "Gear up for an electrifying event at IIIT's fest! Teams of 4 will battle in two rounds: a quiz to secure a franchise spot, followed by a high-stakes player auction. With 100 crores, build your dream team of 12 players while balancing Indian and overseas stars. Master the bidding war, strategize your picks, and aim for the highest team rating to win. Will you outsmart the competition? Join the thrill and showcase your cricketing genius!" },
+
+      { name: "Event 4", text: "Unleash your creativity in this week-long Instagram marketing showdown! Teams of 4-5 will craft and run a dynamic campaign for a given product, posting daily content, reels, and stories to maximize engagement. After 6 days of strategizing and engaging, teams will pitch their campaign results to a panel of judges. Showcase your skills in analytics, content creation, and storytelling to win exciting rewards! Join the challenge and prove your marketing prowess!" },
+
+      { name: "Event@iitg", text: "Gear up for an electrifying event at IIIT's fest! Teams of 4 will battle in two rounds: a quiz to secure a franchise spot, followed by a high-stakes player auction. With 100 crores, build your dream team of 12 players while balancing Indian and overseas stars. Master the bidding war, strategize your picks, and aim for the highest team rating to win. Will you outsmart the competition? Join the thrill and showcase your cricketing genius!" }
    ];
 
    function startAutoSlide() {
